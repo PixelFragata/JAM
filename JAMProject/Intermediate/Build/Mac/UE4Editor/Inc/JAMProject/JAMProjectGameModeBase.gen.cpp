1e12a48724d603890d542560b447967ee80aabb4
Renato Zane <renatozane90.rz@gmail.com>
2018-11-05 10:31:19 -0200

    Projeto criado

---
+++ JAMProject/Intermediate/Build/Mac/UE4Editor/Inc/JAMProject/JAMProjectGameModeBase.gen.cpp
@@ -0,0 +1,78 @@
+// Copyright 1998-2018 Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/GeneratedCppIncludes.h"
+#include "JAMProject/JAMProjectGameModeBase.h"
+#ifdef _MSC_VER
+#pragma warning (push)
+#pragma warning (disable : 4883)
+#endif
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+void EmptyLinkFunctionForGeneratedCodeJAMProjectGameModeBase() {}
+// Cross Module References
+	JAMPROJECT_API UClass* Z_Construct_UClass_AJAMProjectGameModeBase_NoRegister();
+	JAMPROJECT_API UClass* Z_Construct_UClass_AJAMProjectGameModeBase();
+	ENGINE_API UClass* Z_Construct_UClass_AGameModeBase();
+	UPackage* Z_Construct_UPackage__Script_JAMProject();
+// End Cross Module References
+	void AJAMProjectGameModeBase::StaticRegisterNativesAJAMProjectGameModeBase()
+	{
+	}
+	UClass* Z_Construct_UClass_AJAMProjectGameModeBase_NoRegister()
+	{
+		return AJAMProjectGameModeBase::StaticClass();
+	}
+	struct Z_Construct_UClass_AJAMProjectGameModeBase_Statics
+	{
+		static UObject* (*const DependentSingletons[])();
+#if WITH_METADATA
+		static const UE4CodeGen_Private::FMetaDataPairParam Class_MetaDataParams[];
+#endif
+		static const FCppClassTypeInfoStatic StaticCppClassTypeInfo;
+		static const UE4CodeGen_Private::FClassParams ClassParams;
+	};
+	UObject* (*const Z_Construct_UClass_AJAMProjectGameModeBase_Statics::DependentSingletons[])() = {
+		(UObject* (*)())Z_Construct_UClass_AGameModeBase,
+		(UObject* (*)())Z_Construct_UPackage__Script_JAMProject,
+	};
+#if WITH_METADATA
+	const UE4CodeGen_Private::FMetaDataPairParam Z_Construct_UClass_AJAMProjectGameModeBase_Statics::Class_MetaDataParams[] = {
+		{ "HideCategories", "Info Rendering MovementReplication Replication Actor Input Movement Collision Rendering Utilities|Transformation" },
+		{ "IncludePath", "JAMProjectGameModeBase.h" },
+		{ "ModuleRelativePath", "JAMProjectGameModeBase.h" },
+		{ "ShowCategories", "Input|MouseInput Input|TouchInput" },
+	};
+#endif
+	const FCppClassTypeInfoStatic Z_Construct_UClass_AJAMProjectGameModeBase_Statics::StaticCppClassTypeInfo = {
+		TCppClassTypeTraits<AJAMProjectGameModeBase>::IsAbstract,
+	};
+	const UE4CodeGen_Private::FClassParams Z_Construct_UClass_AJAMProjectGameModeBase_Statics::ClassParams = {
+		&AJAMProjectGameModeBase::StaticClass,
+		DependentSingletons, ARRAY_COUNT(DependentSingletons),
+		0x009002A8u,
+		nullptr, 0,
+		nullptr, 0,
+		nullptr,
+		&StaticCppClassTypeInfo,
+		nullptr, 0,
+		METADATA_PARAMS(Z_Construct_UClass_AJAMProjectGameModeBase_Statics::Class_MetaDataParams, ARRAY_COUNT(Z_Construct_UClass_AJAMProjectGameModeBase_Statics::Class_MetaDataParams))
+	};
+	UClass* Z_Construct_UClass_AJAMProjectGameModeBase()
+	{
+		static UClass* OuterClass = nullptr;
+		if (!OuterClass)
+		{
+			UE4CodeGen_Private::ConstructUClass(OuterClass, Z_Construct_UClass_AJAMProjectGameModeBase_Statics::ClassParams);
+		}
+		return OuterClass;
+	}
+	IMPLEMENT_CLASS(AJAMProjectGameModeBase, 2012692924);
+	static FCompiledInDefer Z_CompiledInDefer_UClass_AJAMProjectGameModeBase(Z_Construct_UClass_AJAMProjectGameModeBase, &AJAMProjectGameModeBase::StaticClass, TEXT("/Script/JAMProject"), TEXT("AJAMProjectGameModeBase"), false, nullptr, nullptr, nullptr);
+	DEFINE_VTABLE_PTR_HELPER_CTOR(AJAMProjectGameModeBase);
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+#ifdef _MSC_VER
+#pragma warning (pop)
+#endif
---
+++ JAMProject/Intermediate/Build/Mac/UE4Editor/Inc/JAMProject/JAMProjectGameModeBase.generated.h
@@ -0,0 +1,90 @@
+// Copyright 1998-2018 Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef JAMPROJECT_JAMProjectGameModeBase_generated_h
+#error "JAMProjectGameModeBase.generated.h already included, missing '#pragma once' in JAMProjectGameModeBase.h"
+#endif
+#define JAMPROJECT_JAMProjectGameModeBase_generated_h
+
+#define JAMProject_Source_JAMProject_JAMProjectGameModeBase_h_15_RPC_WRAPPERS
+#define JAMProject_Source_JAMProject_JAMProjectGameModeBase_h_15_RPC_WRAPPERS_NO_PURE_DECLS
+#define JAMProject_Source_JAMProject_JAMProjectGameModeBase_h_15_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesAJAMProjectGameModeBase(); \
+	friend struct Z_Construct_UClass_AJAMProjectGameModeBase_Statics; \
+public: \
+	DECLARE_CLASS(AJAMProjectGameModeBase, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient), CASTCLASS_None, TEXT("/Script/JAMProject"), NO_API) \
+	DECLARE_SERIALIZER(AJAMProjectGameModeBase)
+
+
+#define JAMProject_Source_JAMProject_JAMProjectGameModeBase_h_15_INCLASS \
+private: \
+	static void StaticRegisterNativesAJAMProjectGameModeBase(); \
+	friend struct Z_Construct_UClass_AJAMProjectGameModeBase_Statics; \
+public: \
+	DECLARE_CLASS(AJAMProjectGameModeBase, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient), CASTCLASS_None, TEXT("/Script/JAMProject"), NO_API) \
+	DECLARE_SERIALIZER(AJAMProjectGameModeBase)
+
+
+#define JAMProject_Source_JAMProject_JAMProjectGameModeBase_h_15_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API AJAMProjectGameModeBase(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(AJAMProjectGameModeBase) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, AJAMProjectGameModeBase); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AJAMProjectGameModeBase); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API AJAMProjectGameModeBase(AJAMProjectGameModeBase&&); \
+	NO_API AJAMProjectGameModeBase(const AJAMProjectGameModeBase&); \
+public:
+
+
+#define JAMProject_Source_JAMProject_JAMProjectGameModeBase_h_15_ENHANCED_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API AJAMProjectGameModeBase(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()) : Super(ObjectInitializer) { }; \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API AJAMProjectGameModeBase(AJAMProjectGameModeBase&&); \
+	NO_API AJAMProjectGameModeBase(const AJAMProjectGameModeBase&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, AJAMProjectGameModeBase); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AJAMProjectGameModeBase); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(AJAMProjectGameModeBase)
+
+
+#define JAMProject_Source_JAMProject_JAMProjectGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET
+#define JAMProject_Source_JAMProject_JAMProjectGameModeBase_h_12_PROLOG
+#define JAMProject_Source_JAMProject_JAMProjectGameModeBase_h_15_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	JAMProject_Source_JAMProject_JAMProjectGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET \
+	JAMProject_Source_JAMProject_JAMProjectGameModeBase_h_15_RPC_WRAPPERS \
+	JAMProject_Source_JAMProject_JAMProjectGameModeBase_h_15_INCLASS \
+	JAMProject_Source_JAMProject_JAMProjectGameModeBase_h_15_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define JAMProject_Source_JAMProject_JAMProjectGameModeBase_h_15_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	JAMProject_Source_JAMProject_JAMProjectGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET \
+	JAMProject_Source_JAMProject_JAMProjectGameModeBase_h_15_RPC_WRAPPERS_NO_PURE_DECLS \
+	JAMProject_Source_JAMProject_JAMProjectGameModeBase_h_15_INCLASS_NO_PURE_DECLS \
+	JAMProject_Source_JAMProject_JAMProjectGameModeBase_h_15_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID JAMProject_Source_JAMProject_JAMProjectGameModeBase_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
